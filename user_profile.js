bb17a4e4b38895a232dcacf7f677d314d27ca829
Wes Gelpi <wes.gelpi@icloud.com>
2022-04-25 23:09:22 -0400

    Update user_profile.js

--- user_profile.js
+++ user_profile.js
@@ -109,3 +109,83 @@ $w.onReady(function () {
     })
   
 });
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function buttonSave_click(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+  //let selfTapeDate = Date.now();
+  let selfTapeDate = wixTimestamp();
+  //let selfTapeDate = getTimestamp();
+  $w("#datasetSelfTapeParticipant").setFieldValue("selfTapesUpdateDate", new Date(selfTapeDate));
+  $w("#datasetSelfTapeParticipant").save();
+
+  rankLeaderboard();
+
+}
+function wixTimestamp () {
+  var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  var d = new Date();
+  var curr_date = d.getDate();
+  var curr_month = monthNames[d.getMonth()];
+  var curr_year = d.getFullYear();
+  var curr_hr = d.getHours();
+  var curr_ampm = "";
+  if(curr_hr > 12) {
+    curr_hr = curr_hr - 12
+    curr_ampm = "PM"
+  } else if (curr_hr == 12) {
+    curr_ampm = "PM"
+  } else {
+    curr_ampm = "AM"
+  }
+  var curr_min = d.getMinutes();
+  var curr_time = d.getTime();
+
+  //mmmm dd, yyyy hh:mm pm
+
+  return curr_month + " " + curr_date + ", " + curr_year + " " + curr_hr + ":" + curr_min + " " + curr_ampm;
+}
+
+function rankLeaderboard() { 
+
+	wixData.query("SelfTapeMayParticipationData")
+		.descending("selfTapes")
+		.ascending("selfTapesUpdateDate")
+		.find()
+		.then( (results) => {
+    		if(results.items.length > 0) {
+				for(let i = 0; i < results.items.length; i++) {
+					let toUpdate = {
+						"_id": results.items[i]._id,
+						"leaderBoardRank": i + 1,
+						"selfTapes": results.items[i].selfTapes,
+						"instagramHandle": results.items[i].instagramHandle,
+						"market": results.items[i].market,
+						"witcherSides": results.items[i].witcherSides,
+						"emailAddress": results.items[i].emailAddress,
+						"participatedBefore": results.items[i].participatedBefore,
+						"yearsParticipatedBefore": results.items[i].yearsParticipatedBefore,
+						"recorded16Before": results.items[i].recorded16Before,
+						"yearsRecorded16Before": results.items[i].yearsRecorded16Before,
+						"selfTapesUpdateDate": results.items[i].selfTapesUpdateDate
+					};
+
+					wixData.update("SelfTapeMayParticipationData", toUpdate)
+						.then ( (results) => {
+							let item = results;
+						})
+						.catch( (err) => {
+							let errorMsg = err;
+						})
+				}
+    		} else {
+     			// handle case where no matching items found
+			}
+		} )
+
+}

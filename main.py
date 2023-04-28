2800d20fe8a89747c06ab3ab37880b401ca924b0
JHGelpi <wes.gelpi@icloud.com>
2023-04-27 21:38:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import subprocess
 pythonFile = ''
 folderLocation = 'D:\\github_projects\\selftapemay\\'
 '''Order:
+Initial file needs to be named 'dataset_instagram-api-scraper.json' and located in 'D:\\Nextcloud\\Consulting\\selfTapeMay\\'
 1) instagramJSONParser.py
 2) hashtagParser.py
 3) jsonOuputToCSV.py'''

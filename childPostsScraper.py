df797a26d36e9742ffde2d316784749d1950a214
JHGelpi <wes.gelpi@icloud.com>
2023-05-07 22:53:17 -0400

    Update childPostsScraper.py

--- childPostsScraper.py
+++ childPostsScraper.py
@@ -1,4 +1,5 @@
 import json
+import chardet
 
 # file names
 input_file = 'dataset_instagram-api-scraper_kaytra.json'
@@ -10,13 +11,22 @@ file_path = 'D:\\Nextcloud\\Consulting\\selfTapeMay\\'
 # Mac filepath
 #file_path = '/Users/wegelpi/Nextcloud/Consulting/selfTapeMay/'
 
+# Determine encoding
+def determine_encoding(file_path):
+    with open(file_path, 'rb') as file:
+        result = chardet.detect(file.read())
+    return result['encoding']
+
+# Determine the encoding type of the csv file
+encoding = determine_encoding(file_path + input_file)
+
 # Load the JSON file
-with open(file_path + input_file) as f:
+with open(file_path + input_file, 'r', encoding=encoding) as f:
     data = json.load(f)
 
 # Extract the data from the third level
-child_posts = data['latestPosts']['0']['childPosts']
-
-# Print the extracted data
-print(child_posts)
-
+for post in data[0]["latestPosts"]:
+    for child_post in post["childPosts"]:
+        print(child_post["id"])
+        print(child_post["type"])
+        print("-------------")

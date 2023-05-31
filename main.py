0022e83ab915b5031e244f8d8e7c7fc11a7660c7
JHGelpi <wes.gelpi@icloud.com>
2023-05-31 17:55:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import datetime
 import subprocess
 
 pythonFile = ''
+pythonInterpreter = "/usr/bin/python3"  # Replace with the actual path to the Python interpreter
 
 # Windows filepath
-folderLocation = 'D:\\github_projects\\selftapemay\\'
+#folderLocation = 'D:\\github_projects\\selftapemay\\'
 
 # Mac filepath
 #folderLocation = '/Users/wegelpi/Nextcloud/Consulting/selfTapeMay/' 
-#folderLocation = '/Users/wegelpi/github_repos/selftapemay/'
+folderLocation = '/Users/wegelpi/github_repos/selftapemay/'
 
 '''Order:
 Initial file needs to be named 'dataset_instagram-api-scraper.json' and located in 'D:\\Nextcloud\\Consulting\\selfTapeMay\\'
@@ -22,27 +23,43 @@ Initial file needs to be named 'dataset_instagram-api-scraper.json' and located 
 pythonFile = 'instagramJSONParser.py'
 now = datetime.datetime.now()
 print(now, ": Executing ", pythonFile, "...")
-subprocess.run(['python', folderLocation + pythonFile])
+#Windows
+#subprocess.run(['python', folderLocation + pythonFile])
+
+#MacOS
+subprocess.run([pythonInterpreter, folderLocation + pythonFile])
 now = datetime.datetime.now()
 print(now, ": ", pythonFile, "Completed!")
 
 pythonFile = 'hashtagParser.py'
 now = datetime.datetime.now()
 print(now, ": Executing ", pythonFile, "...")
-subprocess.run(['python', folderLocation + pythonFile])
+#Windows
+#subprocess.run(['python', folderLocation + pythonFile])
+
+#MacOS
+subprocess.run([pythonInterpreter, folderLocation + pythonFile])
 now = datetime.datetime.now()
 print(now, ": ", pythonFile, "Completed!")
 
 pythonFile = 'jsonOuputToCSV.py'
 now = datetime.datetime.now()
 print(now, ": Executing ", pythonFile, "...")
-subprocess.run(['python', folderLocation + pythonFile])
+#Windows
+#subprocess.run(['python', folderLocation + pythonFile])
+
+#MacOS
+subprocess.run([pythonInterpreter, folderLocation + pythonFile])
 now = datetime.datetime.now()
 print(now, ": ", pythonFile, "Completed!")
 
 pythonFile = 'urlDiffs.py'
 now = datetime.datetime.now()
 print(now, ": Executing ", pythonFile, "...")
-subprocess.run(['python', folderLocation + pythonFile])
+#Windows
+#subprocess.run(['python', folderLocation + pythonFile])
+
+#MacOS
+subprocess.run([pythonInterpreter, folderLocation + pythonFile])
 now = datetime.datetime.now()
 print(now, ": ", pythonFile, "Completed!")

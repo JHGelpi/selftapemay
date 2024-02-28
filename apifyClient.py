22ad5bda3f3bd76dfa63f2f606f8ce6f04dcc491
Wes Gelpi <wes.gelpi@icloud.com>
2024-02-27 20:28:20 -0500

    work on apifyClient.py

--- apifyClient.py
+++ apifyClient.py
@@ -10,7 +10,8 @@ from apify_client import ApifyClient
 
 # user variable from bigquery_client.py
 #user = bigqueryUser
-user = "audreyhelpsactorspodcast"
+#user = "audreyhelpsactorspodcast"
+user = "tikka.thedog"
 jsonResult = ""
 
 # Apify URL build
@@ -22,7 +23,7 @@ actor_id = "apify~instagram-reel-scraper/run-sync"
 apiURL = apiURL + actor_id + "?token="
 
 with open(filePath, 'r') as file:
-    fileContent = file.read()
+    fileContent = file.read().strip()
 
 apiURL = apiURL + fileContent
 
@@ -34,8 +35,8 @@ def scrape_instagram(user):
     # Function to call Apify API and scrape Instagram data
     # Build the Apify API Payload
     run_input = {
-        "username": ["audreyhelpsactorspodcast"],
-        "resultsLimit": 5,
+        "username": [user],
+        "resultsLimit": 10,
     }
 
     try:
@@ -44,13 +45,24 @@ def scrape_instagram(user):
 
         # Fetch and print Actor results from the run's dataset (if there are any)
         for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-            print(item)
+            # Desired fields
+            # id, type, ownerUsername, shortURL, timestamp, childPosts
+            # print(item)
+            print('====BEGIN====')
+            print('id: ', item['id'])
+            print('Type: ', item['type'])
+            print('ownerUsername: ', item['ownerUsername'])
+            print('hashtags: ', item['hashtags'])
+            print('shortURL: ', item['url'])
+            print('timestamp: ', item['timestamp'])
+            print('childPosts[]: ', item['childPosts'])
+            print('****END****')
         #response = requests.post(apiURL, json=payload, headers=headers)
         #response.raise_for_status()
-        return run.json()
+        return run
     except requests.exceptions.RequestException as e:
         print(f"Error while calling Apify API: {e}")
         return None
 
 jsonResult = scrape_instagram(user)
-print (jsonResult)
+#print (jsonResult)

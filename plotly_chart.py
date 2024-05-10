c5b635ffda55d979694cf96c526792dc4b841b75
Wes Gelpi <wes.gelpi@icloud.com>
2024-05-09 22:30:55 -0400

    created plotly charts v1

---
+++ plotly_chart.py
@@ -0,0 +1,97 @@
+#Plotly
+from google.cloud import bigquery
+from datetime import datetime
+import pandas as pd
+import plotly.express as px
+
+def gcp_data(query):
+    # Initialize the BigQuery client
+    project_id = 'self-tape-may'
+    client = bigquery.Client(project=project_id)
+
+    #query = 
+    query_job = client.query(query)  # Make an API request.
+
+    try:
+        # Convert query result to a DataFrame
+        gcpdata = query_job.result().to_dataframe()  # Waits for the query to finish
+        return gcpdata
+    except Exception as e:
+        print("Error in gcp_data:", e)
+        return None
+
+#df = gcpdata
+
+def visualize_data_line(df, xaxis, yaxis, charttitle):
+    if df is not None:
+        #print(df.columns)  # Print the DataFrame columns to debug
+        # Assuming 'timestamp' and 'id' are correct, update if necessary
+        fig = px.line(df, x=xaxis, y=yaxis, title=charttitle)
+        fig.show()
+
+def visualize_data_col(df, xaxis, yaxis, charttitle):
+    if df is not None:
+        #print(df.columns)  # Print the DataFrame columns to debug
+        # Assuming 'timestamp' and 'id' are correct, update if necessary
+        fig = px.bar(df, x=xaxis, y=yaxis, title=charttitle)
+        fig.show()
+
+## Query data - Daily posts
+#gcpdata = gcp_data("""
+#        SELECT DATE(timestamp) AS date, COUNT(id) AS id_count FROM `self-tape-may.self_tape_may_data.tblInstagramData` GROUP BY date ORDER BY date;
+#        """)
+
+gcpdata = gcp_data("""SELECT
+  date,
+  id_count,
+  SUM(id_count) OVER (ORDER BY date) AS cumulative_id_count,
+  SUM(camp_count) OVER (ORDER BY date) AS cumulative_camp_count
+FROM (
+  SELECT
+    DATE(timestamp) AS date,
+    COUNT(id) AS id_count,
+    COUNTIF(campaignFlag = True) AS camp_count
+  FROM
+    `self-tape-may.self_tape_may_data.tblInstagramData`
+  GROUP BY
+    date
+  ORDER BY
+    date
+)
+ORDER BY
+  date;""")
+
+# Visualize - Daily posts
+visualize_data_line(gcpdata, 'date', 'id_count', 'Posts by Day')
+
+# Visualize - Cumulative posts
+visualize_data_line(gcpdata, 'date', 'cumulative_id_count', 'Cumulative by Day')
+
+# Visualize - Cumulative posts
+#visualize_data(gcpdata, 'date', 'cumulative_camp_count', 'Cumulative Campaign by Day')
+
+gcpdata = gcp_data("""SELECT
+  market,
+  id_count,
+  SUM(id_count) OVER (ORDER BY market) AS cumulative_id_count,
+  SUM(camp_count) OVER (ORDER BY market) AS cumulative_camp_count
+FROM (
+  SELECT
+    CASE
+      WHEN p.market IS NULL THEN 'No Market'
+      ELSE p.market
+    END AS market,
+    COUNT(i.id) AS id_count,
+    COUNTIF(i.campaignFlag = True) AS camp_count
+  FROM
+    `self-tape-may.self_tape_may_data.tblInstagramData` i
+  JOIN
+    `self-tape-may.self_tape_may_data.tblSTMParticipantData` p ON p.Instagram = i.ownerUsername
+  GROUP BY
+    market
+)
+ORDER BY
+  market;""")
+
+# Visualize - Cumulative posts by Market
+visualize_data_col(gcpdata, 'market', 'cumulative_id_count', 'Cumulative by Market')

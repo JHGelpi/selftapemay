5986ee474403e621de8b85806b751f199d39f537
Wes Gelpi <wes.gelpi@icloud.com>
2024-05-14 06:26:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,16 @@
 
 import bigquery_client
 import plotly_chart
-#import datetime
+import datetime
 #import subprocess
 
 def main():
     #project_id = 'self-tape-may'
+    print("starting to run bigquery_client.get_users() at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     users = bigquery_client.get_users()
+    print("Completed bigquery_client.get_users() at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "running plotly_chart...")
     plotly_chart.plotly_main()
-
+    print("plotly_chart completed at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 #def gen_plotly():
 #    python_file = 'plotly_chart.py'
 #    folderLocation = '/home/wesgelpi/github/selftapemay/'

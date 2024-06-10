c84b4ac3388fcca567ccfefdb9681180de55ee5d
Wes Gelpi <wes.gelpi@icloud.com>
2024-06-09 21:43:36 -0400

    cleaned up some comments

--- main.py
+++ main.py
@@ -10,19 +10,12 @@
 import bigquery_client
 import plotly_chart
 from datetime import datetime
-#import subprocess
 
 def main():
-    #project_id = 'self-tape-may'
-    #print("starting to run bigquery_client.get_users() at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     users = bigquery_client.get_users()
     print("Completed bigquery_client.get_users() at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "running plotly_chart...")
     plotly_chart.plotly_main()
     print("plotly_chart completed at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-#def gen_plotly():
-#    python_file = 'plotly_chart.py'
-#    folderLocation = '/home/wesgelpi/github/selftapemay/'
-#    subprocess.run(['python', folderLocation + python_file])
 
 if __name__ == "__main__":
     main()
